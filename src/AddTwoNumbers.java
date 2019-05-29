ad7494c37adbd24e7ea0fa4df06a65d3cefdc95c
haijingd <haijingd@amazon.com>
2019-05-29 19:03:52 +0800

    leetcode445

--- src/AddTwoNumbers.java
+++ src/AddTwoNumbers.java
@@ -1,3 +1,4 @@
+//2 两数之和
 public class AddTwoNumbers {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         if(l1 == null){
@@ -56,6 +57,30 @@ public class AddTwoNumbers {
             maxhead.next = node;
         }
         return l1;
+
+    }
+
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode s = head;
+        int sum = 0;
+        while (l1 != null || l2 != null){
+            sum = sum/10;
+            if(l1 != null){
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            head.next = new ListNode(sum % 10);
+            head = head.next;
+        }
+        if(sum / 10 == 1){
+            head.next = new ListNode(1);
+        }
+        return s.next;
 
     }
 }
--- src/AddTwoNumbers2.java
+++ src/AddTwoNumbers2.java
@@ -1,7 +1,15 @@
+//445 两数之和2
 public class AddTwoNumbers2 {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+
+        l1 = this.reverse(l1);
+        l2 = this.reverse(l2);
+
         ListNode head = new ListNode(0);
-        ListNode s = head;
+        ListNode dummy = head;
         int sum = 0;
         while (l1 != null || l2 != null){
             sum = sum/10;
@@ -19,7 +27,23 @@ public class AddTwoNumbers2 {
         if(sum / 10 == 1){
             head.next = new ListNode(1);
         }
-        return s.next;
+        return reverse(dummy.next);
+    }
 
+    private ListNode reverse(ListNode head) {
+        ListNode pre, cur, next,dummy;
+        dummy = new ListNode(0);
+        dummy.next = head;
+        pre = head;
+        cur = head.next;
+
+        while (cur != null){
+            next = cur.next;
+            pre.next = next;
+            cur.next = dummy.next;
+            dummy.next = cur;
+            cur = next;
+        }
+        return dummy.next;
     }
 }
